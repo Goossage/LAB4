c9c0d7a0a5a8a225a6fecd66e548ba9abed4b191
Gooss <igorgooss@gmail.com>
2024-05-28 19:44:03 +0300

    Добавлены тестовые данные

--- main-project/main-project.cpp
+++ main-project/main-project.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #9. Library Subscription\n";
     cout << "Author: Igor Oreh\n";
+    cout << "Group: 23AI1d\n";
     return 0;
 }
